b2143440deb0973a4fd17ca9f7e17e2de2f6b9ab
video <videocreator@outlook.de>
2024-01-22 11:59:21 +0100

    Improved server error message; Added basic server functionality (actually serving now)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import socketserver
+import threading
 from http.server import BaseHTTPRequestHandler
 
 
@@ -13,10 +15,27 @@ class YoutubeDownloadHandler(BaseHTTPRequestHandler):
                 self.send_header("Content-Type", "text/plain")
                 self.end_headers()
                 self.wfile.write(
-                    f"The server experienced an unexpected error while processing your request.\nPlease report this on our GitHub issues page and try again later.\n{type(e).__name__}: {str(e)}".encode()
+                    f"The server experienced an unexpected error while processing your request.\nPlease report this on "
+                    f"our GitHub issues page and try again later.\n"
+                    f"https://github.com/baerchen201/testProject_learning_git/issues/new\n"
+                    f"{type(e).__name__}: {str(e)}".encode()
                 )
             except:
                 pass
 
     def do_GET(self):
         pass
+
+
+if __name__ == "__main__":
+    server = socketserver.ThreadingTCPServer(("127.0.0.1", 8081), YoutubeDownloadHandler)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    try:
+        while 1:
+            server_thread.join(0.5)
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+        server.shutdown()
+        server_thread.join()
+    print("Server closed")

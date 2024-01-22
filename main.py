07ccc182cb760a5fd80c6676747b1d9cc8abdd5d
video <videocreator@outlook.de>
2024-01-22 13:14:00 +0100

    Added full download functionality Slow, but works.

--- main.py
+++ main.py
@@ -51,17 +51,17 @@ class YoutubeDownloadHandler(BaseHTTPRequestHandler):
             self.send_response(403)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write("Video is private".encode())
+            self.wfile.write(f"Video is private ({video_id})".encode())
         except pytube.exceptions.VideoRegionBlocked:
             self.send_response(502)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write("Video unavailable in the region of the server".encode())
+            self.wfile.write(f"Video unavailable in the region of the server ({video_id})".encode())
         except pytube.exceptions.VideoUnavailable:
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write("Video can not be accessed".encode())
+            self.wfile.write(f"Video can not be accessed ({video_id})".encode())
         if not _:
             return
         if not streams:
@@ -74,7 +74,7 @@ class YoutubeDownloadHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write("The requested stream id is out of range".encode())
+            self.wfile.write(f"The requested stream id is out of range, only {len(streams)} streams available".encode())
             return
         else:
             stream: pytube.Stream = streams[stream_id]
@@ -82,23 +82,40 @@ class YoutubeDownloadHandler(BaseHTTPRequestHandler):
             head = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
             }
+            head.update({"Range": self.headers.get("Range")} if "Range" in self.headers else {})
             ggl_conn: http.client.HTTPResponse = urllib.request.urlopen(urllib.request.Request(url, headers=head))
             if ggl_conn.getcode() not in (200, 206):
                 self.send_response(502)
-                for header, content in ggl_conn.getheaders():
-                    self.send_header(header, content)
+                for header in ggl_conn.getheaders():
+                    self.send_header(*header)
                 self.end_headers()
                 self.wfile.write("Non-success status code".encode())
                 self.send_response(ggl_conn.getcode())
-                for header, content in ggl_conn.getheaders():
-                    self.send_header(header, content)
+                for header in ggl_conn.getheaders():
+                    self.send_header(*header)
                 self.end_headers()
                 self.wfile.write(ggl_conn.read(4096))
                 return
+            else:
+                self.send_response(ggl_conn.getcode())
+                for header in ggl_conn.getheaders():
+                    if header[0] == "Content-Type":
+                        self.send_header("Content-Type", stream.mime_type)
+                        continue
+                    elif header[0] == "Content-Disposition":
+                        self.send_header("Content-Disposition", f"attachment;filename={stream.default_filename}")
+                        continue
+                    self.send_header(*header)
+                self.end_headers()
+                while 1:
+                    chunk = ggl_conn.read(4096)
+                    if not chunk:
+                        break
+                    self.wfile.write(chunk)
 
 
 if __name__ == "__main__":
-    server = socketserver.ThreadingTCPServer(("127.0.0.1", 8081), YoutubeDownloadHandler)
+    server = socketserver.ThreadingTCPServer(("0.0.0.0", 8081), YoutubeDownloadHandler)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.start()
     try:
